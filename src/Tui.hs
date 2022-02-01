2c5042bd01f95faf9bf22c0cba378cce8e0d6fea
adrian13579 <adrianportales135@gmail.com>
2022-01-31 22:40:50 -0500

    Print Environment in form of squared

--- src/Tui.hs
+++ src/Tui.hs
@@ -3,9 +3,23 @@ module Tui where
 import Environment
 import Objects
 
-getRow r e = [charObject (r, c) | c <- [1 .. 3]]
+printPrettyEnvironment env =
+  let envMap = concat [getRow r | r <- [0 .. fst $ dimension env]]
+      envTime = "time: " ++ show (time env) ++ "\n"
+      envSeed = "seed: " ++ show (seed env) ++ "\n"
+   in envMap ++ envTime ++ envSeed
   where
-    charObject c =
-      let o = objectsAt c e
-       in case o of
-            [Kid a] -> 'K'
+    getRow r = concat [charObject (r, c) | c <- [0 .. snd $ dimension env]] ++ "\n"
+    charObject coord =
+      let charObjects = concat [f x | x <- objectsAt coord env]
+       in case length charObjects of
+            0 -> "    " ++ show coord ++ " "
+            1 -> " " ++ charObjects ++ "  " ++ show coord ++ " "
+            2 -> " " ++ charObjects ++ " " ++ show coord ++ " "
+            _ -> " " ++ charObjects ++ show coord ++ " "
+    f o = case o of
+      Kid _ -> "K"
+      Obstacle _ -> "O"
+      Dirt _ -> "D"
+      Robot {} -> "R"
+      Playpen _ -> "P"
