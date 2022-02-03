3ec687e4ee1bf6fbc89da4a49a023db2d825ab8a
adrian13579 <adrianportales135@gmail.com>
2022-02-03 01:11:03 -0500

    Refactor main

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,7 @@
 module Main where
 
-import Debug.Trace
 import Environment
-import Objects
 import Simulation
-import Tui
-import Data.Matrix
-import Robots
-
-{- main :: IO ()
-main = putStr $ printPrettyEnvironment (kidAction (Kid (0, 0)) initEnv) -}
-
-{- main :: IO ()
-main = print $ scanEnv (0, 0) initEnv   -}
-
-{- main :: IO ()
-main = print $ adjacentCoords (2, 2)-- initEnv  -}
 
 main :: IO ()
-main = print $ findPath (2,4) (0,0) ( scanEnv (0, 0) initEnv ) initEnv []  
-
--- main = print $ adjacentEmptyCoords (2,4) initEnv
-
-
+main = print $ runSimulation initEnv 500
