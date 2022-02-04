0ff80c9da7e65efebadb98789cc4024213ec68c6
adrian13579 <adrianportales135@gmail.com>
2022-02-03 21:23:00 -0500

    Improve map visualization

--- src/Tui.hs
+++ src/Tui.hs
@@ -2,27 +2,30 @@ module Tui where
 
 import Environment
 import Objects
+import Utils
 
 printPrettyEnvironment env =
   let envMap = concat [getRow r | r <- [0 .. fst (dimension env) - 1]]
       envTime = "time: " ++ show (time env) ++ "\n"
-      -- envSeed = "seed: " ++ show (seed env) ++ "\n"
-   in envMap ++ envTime -- ++ envSeed
+      clean = "clean: " ++ show (round (100 - percent part total)) ++ "%"
+   in envMap ++ envTime ++ clean
   where
+    total = let (m, n) = dimension env in fromIntegral (m * n)
+    part = fromIntegral $ length (dirt env)
     getRow r = concat [charObject (r, c) | c <- [0 .. snd (dimension env) - 1]] ++ "\n"
     charObject coord =
       let charObjects = concat [f x | x <- objectsAt coord env]
        in case length charObjects of
-            0 -> "    "  ++ "|"
-            1 -> " " ++ charObjects ++ "  "  ++ "|"
-            2 -> " " ++ charObjects ++ " "  ++ "|"
-            _ -> " " ++ charObjects ++  "|"
+            0 -> "    " ++ "|"
+            1 -> " " ++ charObjects ++ "  " ++ "|"
+            2 -> " " ++ charObjects ++ " " ++ "|"
+            _ -> " " ++ charObjects ++ "|"
     f o = case o of
       Kid _ -> "K"
       Obstacle _ -> "Ob"
       Dirt _ -> "D"
-      Robot AlphaRobot ( Just (Kid _) ) _-> "Ak"
+      Robot AlphaRobot (Just (Kid _)) _ -> "Ak"
       Robot AlphaRobot Nothing _ -> "A"
-      Robot BetaRobot ( Just (Kid _) ) _-> "Bk"
+      Robot BetaRobot (Just (Kid _)) _ -> "Bk"
       Robot BetaRobot Nothing _ -> "B"
       Playpen _ -> "P"
